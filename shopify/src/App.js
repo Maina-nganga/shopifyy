36d8e9ecc8ca755592da6246b31725858f1994a4
Maina Nganga <ngangamaina091@gmail.com>
2025-10-23 11:45:48 +0300

    Revert "Merge pull request #1 from Maina-nganga/Register"
    This reverts commit 9b9c35a015d7f2f0c6664bdd30f19d75949086f3, reversing
    changes made to b84d96f01b238bfbd2af4aaa1ce7b1b40cef7f57.

--- shopify/src/App.js
+++ shopify/src/App.js
@@ -1,40 +1,8 @@
-import React, { useState } from "react";
-import Cart from "./Cart";
-import Register from "./Register";
-import "./App.css";
+import React from 'react';
 
 function App() {
-  const [user, setUser] = useState(null);
-  const [cartItems, setCartItems] = useState([]);
-  const [showRegister, setShowRegister] = useState(false);
-
-  function handleAddToCart(product) {
-    setCartItems((prev) => [...prev, product]);
-  }
-
-  function handleRemoveFromCart(id) {
-    setCartItems((prev) => prev.filter((item) => item.id !== id));
-  }
-
-  if (!user) {
-    return showRegister ? (
-      <Register onRegister={() => setShowRegister(false)} />
-    ) : (
-      <Login
-        onLogin={(userData) => setUser(userData)}
-        onSwitchToRegister={() => setShowRegister(true)}
-      />
-    );
-  }
-
   return (
-    <div className="app">
-      <h1>Welcome, {user.name} </h1>
-      <button onClick={() => setUser(null)}>Logout</button>
-      <div className="content">
-        <ProductList products={products} onAddToCart={handleAddToCart} />
-        <Cart cartItems={cartItems} onRemoveFromCart={handleRemoveFromCart} />
-      </div>
+    <div className="App">
     </div>
   );
 }
